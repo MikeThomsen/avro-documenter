39dfddbf48f1f6d193ecc8245eef3f478f0efe67
Mike Thomsen <mikerthomsen@gmail.com>
2020-08-07 20:42:09 -0400

    Added code to extract embedded record schemas.

--- src/main/java/org/apache/avro/documentation/App.java
+++ src/main/java/org/apache/avro/documentation/App.java
@@ -10,6 +10,11 @@ import org.apache.commons.cli.ParseException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class App {
     private static CommandLine parseArguments(String[] args) throws ParseException {
@@ -28,6 +33,48 @@ public class App {
         return parser.parse(options, args);
     }
 
+    public static Optional<List<Schema>> checkForRecord(Schema fieldSchema) {
+        Schema.Type type = fieldSchema.getType();
+        if (type == Schema.Type.RECORD) {
+            return Optional.of(Arrays.asList(fieldSchema));
+        } else if (type == Schema.Type.ARRAY && fieldSchema.getElementType().getType() == Schema.Type.RECORD) {
+            return Optional.of(Arrays.asList(fieldSchema.getElementType()));
+        } else if (type == Schema.Type.MAP && fieldSchema.getValueType().getType() == Schema.Type.RECORD) {
+            return Optional.of(Arrays.asList(fieldSchema.getValueType()));
+        } else if (type == Schema.Type.UNION) {
+            List<Schema> unionTypes = fieldSchema.getTypes();
+            List<Schema> retVal = new ArrayList<>();
+            unionTypes
+                .stream()
+                .map(App::checkForRecord)
+                .forEach(result -> {
+                    if (result.isPresent()) {
+                        retVal.addAll(result.get());
+                    }
+                });
+
+            return retVal.size() > 0 ? Optional.of(retVal) : Optional.empty();
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static void getReferencedRecords(final Schema schema, final List<Schema> referencedRecords) {
+        schema.getFields().forEach(field -> {
+            Optional<List<Schema>> result = checkForRecord(field.schema());
+            if (result.isPresent()) {
+                List<Schema> extracted = result.get();
+                extracted.forEach(extractedSchema -> {
+                    if (!referencedRecords.contains(extractedSchema)) {
+                        referencedRecords.add(extractedSchema);
+                    }
+
+                    getReferencedRecords(schema, referencedRecords);
+                });
+            }
+        });
+    }
+
     public static void main(String[] args) throws Exception {
         CommandLine arguments = parseArguments(args);
         String schema = arguments.getOptionValue("schema");
@@ -40,5 +87,7 @@ public class App {
         }
 
         Schema parsed = new Schema.Parser().parse(new FileInputStream(rawInput));
+        List<Schema> referencedRecords = new ArrayList<>();
+        getReferencedRecords(parsed, referencedRecords);
     }
 }
